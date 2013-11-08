435da9de6d04de453087317ed1e5272e6c71d597
Peter Stark <peter.stark@menmo.se>
2013-11-08 08:27:59 +0100

    updated tests to 2013-2014

--- test/test.py
+++ test/test.py
@@ -10,13 +10,13 @@ import nhl
 class TestNhl(unittest.TestCase):
 
     def test_reader_summary(self):
-        nhl_reader = nhl.reader("20122013", report='summary')
+        nhl_reader = nhl.reader("20132014", report='summary')
         for player in nhl_reader:
             self.assertTrue(player)
 
     
     def test_reader_bios(self):
-        nhl_reader = nhl.reader("20122013", report='bios')
+        nhl_reader = nhl.reader("20132014", report='bios')
         for player in nhl_reader:
             self.assertTrue(player)
 
